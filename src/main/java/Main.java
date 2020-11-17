a16dff6abe763ad6e03849af3b25055a842781d7
Christopher-Schmitz <C.Schmitz.19@hhu.de>
2020-11-17 11:05:39 +0100

    mob next [ci-skip]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.Instant;
 import java.time.LocalDateTime;
-import java.util.TimeZone;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
@@ -20,18 +22,33 @@ public class Main {
 	}
 
 
-	public static void readFile(String dateiname){
+	public static List<Eintrag> readFile(String dateiname){
 		Path p = Path.of(dateiname);
 		try {
 			Stream<String> lineStream = Files.lines(p);
+			AtomicInteger index = new AtomicInteger(0);
+			Collection<List<String>> values = lineStream
+					.collect(Collectors.groupingBy(e -> index.getAndIncrement() / 2))
+					.values();
+
+			List<Eintrag> eintraege = values
+					.stream()
+					.map(l -> new Eintrag(l.get(0), l.get(1)))
+					.collect(Collectors.toList());
+
+			return eintraege;
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		return Collections.emptyList();
 	}
 
 
 	public static void main(String[] args) {
-
+		long start = System.currentTimeMillis();
+		readFile("odyssey.txt");
+		long dauer = System.currentTimeMillis() - start;
+		System.out.println("Dauer: " + dauer);
 	}
 
 
